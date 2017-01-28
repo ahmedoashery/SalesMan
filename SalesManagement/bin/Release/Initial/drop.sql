0bb09a2dc35f8951cbe2435cf5f5b359f055827e
ahmedoashery <ahmedoashery@gmail.com>
2017-01-28 14:03:58 +0200

    Sales Man Last Final Edit And Tested.

--- SalesManagement/bin/Release/Initial/drop.sql
+++
@@ -1,7 +0,0 @@
-USE [master]
-GO
-IF EXISTS(select * from sys.databases where name='products_db') 
-ALTER DATABASE products_db SET ONLINE
-GO
-DROP DATABASE products_db
-GO
--- SalesManagement/bin/Release/Initial/drop.sql.cmd
+++
@@ -1,4 +0,0 @@
-@echooff
-sqlcmd /S .\appserverdb -i drop.sql
-sqlcmd /S .\appserverdb -i products_db_script.sql
-#pause
